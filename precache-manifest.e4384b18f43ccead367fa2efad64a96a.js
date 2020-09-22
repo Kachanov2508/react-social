a3c983ce64e1374b812dd34991626b6a0a07536c
Kachanov2508 <Kachanov2508@yandex.ru>
2020-09-22 17:51:16 +0300

    Updates

---
+++ precache-manifest.e4384b18f43ccead367fa2efad64a96a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c5ee4f7b3615472232668150ed3063c",
+    "url": "/react-social/index.html"
+  },
+  {
+    "revision": "647ae012daf74a9b767d",
+    "url": "/react-social/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "8e65c6d1d2283e501efa",
+    "url": "/react-social/static/js/2.b091b18d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-social/static/js/2.b091b18d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "647ae012daf74a9b767d",
+    "url": "/react-social/static/js/main.e0b304fb.chunk.js"
+  },
+  {
+    "revision": "4d4ba697a6a12f2663bf",
+    "url": "/react-social/static/js/runtime-main.df6aed2a.js"
+  }
+]);
