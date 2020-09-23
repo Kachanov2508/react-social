f525d5d363542255a07a67ab33bf4da796909d13
Kachanov2508 <Kachanov2508@yandex.ru>
2020-09-23 17:30:38 +0300

    ok

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+	return (
+		<header className="header">
+			<img src="https://bower.io/img/bower-logo.png" alt="logo" />
+		</header>
+	);
+};
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Profile = () => {
+	return (
+		<div className="content">
+			<figure>
+				<img
+					src="https://wowslider.net/local-sliders/demo-10/data1/images/road220058.jpg"
+					alt="photo"
+				/>
+			</figure>
+			<div>avatar + description</div>
+			<div>My post</div>
+			<div>New post</div>
+			<div>
+				<div>post 1</div>
+				<div>post 2</div>
+			</div>
+		</div>
+	);
+};
+
+export default Profile;
