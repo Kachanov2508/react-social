845c69a22fd3c932e91577805c528e6d0774e12b
Kachanov2508 <Kachanov2508@yandex.ru>
2020-09-25 12:29:19 +0300

    ok

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import MyPosts from "./MyPosts/MyPosts";
-import classes from "./Profile.module.css";
+// import classes from "./Profile.module.css";
 
 const Profile = () => {
 	return (
-		<div className={classes.content}>
+		<div>
 			<figure>
-				<img src="https://wowslider.net/local-sliders/demo-10/data1/images/road220058.jpg"/>
+				<img src="https://wowslider.net/local-sliders/demo-10/data1/images/road220058.jpg" alt={`qweqwe`}/>
 			</figure>
 			<div>avatar + description</div>
 			<MyPosts />
