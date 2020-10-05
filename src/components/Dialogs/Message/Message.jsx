8a3abb8a440dbe20758ae9aabd17212abf49740e
Kachanov2508 <Kachanov2508@yandex.ru>
2020-10-05 17:59:06 +0300

    ok

--- src/components/Dialogs/Message/Message.jsx
+++ src/components/Dialogs/Message/Message.jsx
@@ -2,19 +2,28 @@ import React from "react";
 import classes from "./Message.module.css";
 
 const Message = (props) => {
+	let message = React.createRef();
 
-	let message = React.createRef()
-	
 	const setMessage = () => {
 		let text = message.current.value;
-		console.log(text)
-	}
+		console.log(text);
+	};
 
 	return (
 		<div className={classes.message}>
 			{/* {props.message} */}
+			<div className={classes.sentMessage}>
+				<ul>
+					<li>Привет</li>
+					<li>Добрый день!</li>
+					<li>Какие курсы можно пройти?</li>
+					<li>Разработка на ReactJS</li>
+				</ul>
+			</div>
 			<textarea placeholder="текст сообщения" ref={message}></textarea>
-			<button className={classes.button} onClick={setMessage}>Отправить</button>
+			<button className={classes.button} onClick={setMessage}>
+				Отправить
+			</button>
 		</div>
 	);
 };
